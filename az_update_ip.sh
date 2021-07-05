ad003d31970eaec49a12aaa38acef75303f06aea
Ubuntu <azureuser@ouline.xls4nfwg2k3uxnven0hwz0zeeb.hx.internal.cloudapp.net>
2021-07-05 18:38:09 +0000

    update azure vm ip script

---
+++ az_update_ip.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+APP_ID=$1
+PAWD=$2
+TENANT_ID=$3
+GROUP=$4
+VMNAME=$5
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
+az login --service-principal -u $APP_ID          --password $PAWD --tenant $TENANT_ID 
+nicid=`az vm nic list -g $GROUP --vm-name $VMNAME --query []."id" -o tsv`
+nicname=`basename $nicid`
+ipconfigName1=`az network nic ip-config list --nic-name $nicname --resource-group $GROUP --query "[?primary].name" -o tsv`
+publicIpId=`az network nic ip-config show --name $ipconfigName1  --nic-name $nicname --resource-group $GROUP  --query "publicIpAddress.id" -o tsv`
+publicIpName=`basename $publicIpId`
+
+#get secondary ip
+ipconfigName2=`az network nic ip-config list --nic-name $nicname --resource-group  $GROUP --query "[?to_string(primary) == 'false'].name" -o tsv`
+publicIpId2=`az network nic ip-config show --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP  --query "publicIpAddress.id" -o tsv`
+publicIpName2=`basename $publicIpId2`
+ip2=`az network public-ip show -g $GROUP -n $publicIpName2 --query "ipAddress" -o tsv`
+#register backup ip
+bash update_anodns.sh  `ls *.token | head -n 1` $ip2 
+
+#now change ip1
+
+#az network nic ip-config update --name ipconfig1  --nic-name 236 --resource-group  $GROUP --remove PublicIpAddress
+az network nic ip-config update --name $ipconfigName1  --nic-name $nicname --resource-group $GROUP  --remove PublicIpAddress
+
+#az network nic ip-config update --name ipconfig1  --nic-name 236 --resource-group  $GROUP --public-ip-address "ip"
+az network nic ip-config update --name $ipconfigName1  --nic-name $nicname --resource-group $GROUP --public-ip-address $publicIpName
+
+#register primary ip
+bash update_anodns.sh  `ls *.token | head -n 1`
+
+#now change ip2
+
+#az network nic ip-config update --name ipconfig1  --nic-name 236 --resource-group  $GROUP --remove PublicIpAddress
+az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group  $GROUP --remove PublicIpAddress
+
+#az network nic ip-config update --name ipconfig1  --nic-name 236 --resource-group  $GROUP --public-ip-address "ip"
+az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP --public-ip-address $publicIpName2
