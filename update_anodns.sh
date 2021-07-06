9552e418e1ae5b6c73a2770606367821e0245014
Ubuntu <azureuser@ssr.r13lrdmmytzefmj5jyzyqnn1je.hx.internal.cloudapp.net>
2021-07-06 09:51:00 +0000

    run the script at the script current path

--- update_anodns.sh
+++ update_anodns.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 tokenFile=$1
 ip=$2
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
 [ ! -f "$tokenFile" ] && echo "ERROR: provide a valid token file as parameter"&&exit 1
 token=`cat $tokenFile`
 fqdn=`basename ${tokenFile} '.token'`
