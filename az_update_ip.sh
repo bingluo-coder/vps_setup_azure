44e5a7996a568b22fca418f424348028ce90fd55
Ubuntu <azureuser@ssr.r13lrdmmytzefmj5jyzyqnn1je.hx.internal.cloudapp.net>
2021-07-06 09:20:52 +0000

    adapt to one ipconfig scenario

--- az_update_ip.sh
+++ az_update_ip.sh
@@ -18,11 +18,13 @@ publicIpName=`basename $publicIpId`
 
 #get secondary ip
 ipconfigName2=`az network nic ip-config list --nic-name $nicname --resource-group  $GROUP --query "[?to_string(primary) == 'false'].name" -o tsv`
-publicIpId2=`az network nic ip-config show --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP  --query "publicIpAddress.id" -o tsv`
-publicIpName2=`basename $publicIpId2`
-ip2=`az network public-ip show -g $GROUP -n $publicIpName2 --query "ipAddress" -o tsv`
-#register backup ip
-bash update_anodns.sh  `ls *.token | head -n 1` $ip2 
+if [ ! -z "$ipconfigName2" ]; then
+	publicIpId2=`az network nic ip-config show --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP  --query "publicIpAddress.id" -o tsv`
+	publicIpName2=`basename $publicIpId2`
+	ip2=`az network public-ip show -g $GROUP -n $publicIpName2 --query "ipAddress" -o tsv`
+	#register backup ip
+	bash update_anodns.sh  `ls *.token | head -n 1` $ip2 
+fi
 
 #now change ip1
 
@@ -35,10 +37,12 @@ az network nic ip-config update --name $ipconfigName1  --nic-name $nicname --res
 #register primary ip
 bash update_anodns.sh  `ls *.token | head -n 1`
 
+
 #now change ip2
+if [ ! -z "$ipconfigName2" ]; then
+	#az network nic ip-config update --name ipconfig2  --nic-name nic236 --resource-group  $GROUP --remove PublicIpAddress
+	az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group  $GROUP --remove PublicIpAddress
 
-#az network nic ip-config update --name ipconfig2  --nic-name nic236 --resource-group  $GROUP --remove PublicIpAddress
-az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group  $GROUP --remove PublicIpAddress
-
-#az network nic ip-config update --name ipconfig2  --nic-name nic236 --resource-group  $GROUP --public-ip-address "ip23"
-az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP --public-ip-address $publicIpName2
+	#az network nic ip-config update --name ipconfig2  --nic-name nic236 --resource-group  $GROUP --public-ip-address "ip23"
+	az network nic ip-config update --name $ipconfigName2  --nic-name $nicname --resource-group $GROUP --public-ip-address $publicIpName2
+fi
